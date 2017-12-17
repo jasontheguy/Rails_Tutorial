69bea6b77ccd8ffebd287acc765945af41a02e89
jason <flip_z@localhost.localdomain>
2017-12-17 15:03:03 -0700

    Trying to get routes to work

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-def hello
-  render_html: "Hello world!"
-
-
-
-end
+  def hello
+    puts "Hello"
+  end
 end
